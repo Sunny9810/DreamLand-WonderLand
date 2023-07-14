a163ba5d9d6adc28977c6dcb944a7ca4977b953f
Avani <avani.jadeja@gmail.com>
2023-07-14 23:11:24 +0530

    Added seeds.js file and add categories in seeds file

---
+++ server/config/seeds.js
@@ -0,0 +1,12 @@
+const db = require('./connection');
+const { User, Product, Category } = require('../models');
+
+db.once('open', async () => {
+  await Category.deleteMany();
+
+  const categories = await Category.insertMany([
+    { name: 'Baby' },
+    { name: 'Kids' },
+    { name: 'Grown-Ups' },
+   
+  ]);
