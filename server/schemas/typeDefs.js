ee1df6e45a3f5ebfdf40f91fbca28d572b02649e
Ivan Zavaleta <ivzavaleta@gmail.com>
2023-07-14 00:00:09 -0500

    added models and typedefs to server folder

---
+++ server/schemas/typeDefs.js
@@ -0,0 +1,60 @@
+const { gql } = require('apollo-server-express');
+
+const typeDefs = gql`
+  type Category {
+    _id: ID
+    name: Sizes
+  }
+
+  type Sizes {
+    sizes: [String]
+  }
+
+  type Product {
+    _id: ID
+    name: String
+    description: String
+    image: String
+    quantity: Int
+    price: Float
+    category: Category
+  }
+
+  type Order {
+    _id: ID
+    purchaseDate: String
+    products: [Product]
+  }
+
+  type User {
+    _id: ID
+    firstName: String
+    lastName: String
+    email: String
+    orders: [Order]
+  }
+
+  type Checkout {
+    session: ID
+  }
+
+  type Auth {
+    token: ID
+    user: User
+  }
+
+  type Query {
+    categories: [Category]
+    products(category: ID): [Product]
+    product(_id: ID!): Product
+    user: User
+    order(_id: ID!): Order
+    checkout(products: [ID]!): Checkout
+  }
+
+  type Mutation {
+  
+  }
+`;
+
+module.exports = typeDefs;
