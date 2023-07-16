494c16948f8f4048f670fdca4eff61db5a4c3204
Brodi LeBlanc <brodi.leblanc@gmail.com>
2023-07-16 10:44:58 -0700

    added boilerplate header and navbar code

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,17 @@
-
+import React from 'react';
+import Header from './components/Header';
+import Navbar from './components/Navbar';
+import Footer from './components/Footer';
 import './App.css';
 import MyBook from './components/Hero';
 
 function App() {
   return (
-    <MyBook/>
+    <div>
+      <Header />
+      <MyBook />
+      <Footer />
+      </div>
   );
 }
 
