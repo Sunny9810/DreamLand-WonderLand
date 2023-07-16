1aae521f9f7d9766049441379bdd571ba30b4571
Sara Wendland <info@rosebudsara.com>
2023-07-16 11:28:36 -0500

    Added pretty images of night scene to book pages

--- client/src/components/Hero.js
+++ client/src/components/Hero.js
@@ -7,40 +7,58 @@ import '../styles/hero.css'
 export default function MyBook() {
 
     
+    
   return (
-   
+   <Container>
+    <Card>
     <div className="App">
       <span>
         <div className="flipbook-container">
-          <HTMLFlipBook width={200} height={200} size="stretch" >
-            <div className="page1" >
+          <HTMLFlipBook width={200} height={200} size="stretch"  >
+            <div className="page1 DemoPage" >
               <h1>Jammies</h1>
-              <p>Organic Jammies</p>
+              <p>Organic Jammies page1</p>
               {/* Add a link */}
               <a href="https://rosebudsara.com">shameless self promotion while we figure out linking pages from here LOL</a>
               
             </div>
             <div className="page2 ">
               <h1>More Jammies</h1>
-              <p>My Jammies</p>
+              <p>page 2 My Jammies</p>
             </div>
             <div className="page3">
               <h1>Jammies Jammies all day long</h1>
-              <p>More Jammies for family jammies</p>
+              <p>page 3More Jammies for family jammies</p>
               {/* image  */}
             </div>
             <div className="page4">
-              <h1>My excellent fourth article</h1>
-              <p>My excellent fourth content</p>
+              <h1>Jammiese</h1>
+              <p>pagefour</p>
             </div>
             <div className="page5">
               <h1>Page Flippiee</h1>
-              <p>MFlippie Flip</p>
+              <p>Flippie Flip 5</p>
+            </div>
+            <div className="page6">
+              <h1>Jammiese</h1>
+              <p> 6 products will go on these pages then there will be another product inventory page when you scroll down</p>
+            </div>
+            <div className="page7">
+              <h1>Page Flippiee</h1>
+              <p>7 Flippie Flip</p>
+            </div>
+            <div className="page8">
+              <h1>Page Flippiee</h1>
+              <p>7 Flippie Flip</p>
             </div>
           </HTMLFlipBook>
         </div>
       </span>
     </div>
+
+    </Card>
+
+    </Container>
   
   );
 }
