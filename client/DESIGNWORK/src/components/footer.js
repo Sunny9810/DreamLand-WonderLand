2f0aaa4390a81b121e7adcbd3fa898543bd2ab04
Sara Wendland <info@rosebudsara.com>
2023-07-18 20:10:51 -0500

    resolved merge conflict

---
+++ client/DESIGNWORK/src/components/footer.js
@@ -0,0 +1,13 @@
+import React, { useEffect, useState } from 'react';
+
+/* if you are experiencing an error or VS Code triggers an error. This code is operational on the server */
+
+const Footer = () => {
+  return (
+    <footer className="footer">
+      <p>"This is a footer for the website."</p>
+    </footer>
+ );
+}
+
+export default Footer;
