0b9c6fb34538c0fa63ae633135d63fc0738ed0c4
Gunner Wagoner <gunnerwagoner@hotmail.com>
2023-07-18 19:29:54 -0500

    Revert "Develop"

---
+++ client/src/components/jumbotron/index.js
@@ -0,0 +1,20 @@
+// import react
+import React from "react";
+
+// funcation jumbotron
+function Jumbotron({ children }) {
+  return (
+    <div
+      style={{
+        height: 560,
+        clear: "both",
+        paddingTop: 120,
+        textAlign: "center",
+      }}
+    >
+      {children}
+    </div>
+  );
+}
+
+export default Jumbotron;
