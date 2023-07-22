43fd99ae651ae9487eacaf4d718a3fbb06748eb1
Arevik-Sunny <arevik.abrahamyan18@gmail.com>
2023-07-21 20:01:52 -0400

    add changes

--- client/src/App.js
+++ client/src/App.js
@@ -10,6 +10,7 @@ import { setContext } from "@apollo/client/link/context";
 import { Provider } from "react-redux";
 import store from "./utils/store"
 
+
 import Home from "./pages/Home";
 import Detail from "./pages/Detail";
 import NoMatch from "./pages/NoMatch";
