8e57966dcab64cdf4ae266f75a05b0f911b672d1
Brodi LeBlanc <brodi.leblanc@gmail.com>
2023-07-22 21:45:32 -0700

    I can't remember what I did... went down a rabbit hole

--- client/src/components/Nav/index.js
+++ client/src/components/Nav/index.js
@@ -39,7 +39,12 @@ function MyNavbar() {
   return (
     <Navbar expand={expand} className="bg-body-tertiary mb-3 nav-background">
       <Container fluid>
-        <Navbar.Brand href="/">Dreamland Wonderland</Navbar.Brand>
+      <div className="d-flex justify-content-center align-items-center p-3">
+              <img src={Logo} alt="Dreamland Wonderland Logo" height={50} />
+            </div>
+      <h1>
+        <Navbar.Brand href="/"style={{ fontSize: '3rem', color: 'white' }}>Dreamland Wonderland</Navbar.Brand>
+      </h1>
         <Navbar.Toggle aria-controls={`offcanvasNavbar-expand-${expand}`} />
         <Navbar.Offcanvas
           id={`offcanvasNavbar-expand-${expand}`}
@@ -54,22 +59,20 @@ function MyNavbar() {
           </Offcanvas.Header>
           <Nav className="flex-column">
             <Nav.Link href="/">Home</Nav.Link>
-            <NavDropdown title="Sizes" id="navbarScrollingDropdown">
+            <NavDropdown title="Sizes" id="navbarScrollingDropdown" menuVariant="dark" >
               <NavDropdown.Item href="#action3">Babies</NavDropdown.Item>
               <NavDropdown.Item href="#action4">Kids</NavDropdown.Item>
               <NavDropdown.Item href="#action4">Adults</NavDropdown.Item>
               <NavDropdown.Divider />
               <NavDropdown.Item href="#action5">All</NavDropdown.Item>
             </NavDropdown>
-            <NavDropdown title="Account" id="navbarScrollingDropdown">
+            <NavDropdown title="Account" menuVariant="dark" id="navbarScrollingDropdown">
               <NavDropdown.Item href="/signup">Sign-up</NavDropdown.Item>
               <NavDropdown.Item href="/login">Login</NavDropdown.Item>
               <NavDropdown.Divider />
               <NavDropdown.Item href="/orderHistory">Profile</NavDropdown.Item>
             </NavDropdown>
             <Nav.Link href="/cart">Cart</Nav.Link>
-
-
           </Nav>
         </Navbar.Offcanvas>
       </Container>
