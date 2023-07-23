3a0e9b7a680facec08a765d553c52d3dc16bcb9b
Avani <avani.jadeja@gmail.com>
2023-07-23 09:37:52 +0530

    Change images files names and seed value in categories[2] image

--- server/config/seeds.js
+++ server/config/seeds.js
@@ -1,21 +1,20 @@
-const db = require("./connection");  //* mongoose db import
-const { User, Product, Category } = require("../models");   //* importing models to create the data objects that will be inserted in the live db
+const db = require("./connection"); //* mongoose db import
+const { User, Product, Category } = require("../models"); //* importing models to create the data objects that will be inserted in the live db
 
-db.once("open", async () => {   //! db.once listens for the connection to the db to open and executes the embedded logic
+db.once("open", async () => {
+  //! db.once listens for the connection to the db to open and executes the embedded logic
   await Category.deleteMany();
 
   const categories = await Category.insertMany([
     {
       name: "Baby(0-24M)",
       size: ["NB", "0-3", "3-6", "6-9", "9-12", "12-18", "18-24"],
-      
     },
     {
       name: "Kids(2-14Y)",
       size: ["2-3", "4-5", "6-7", "8-9", "10", "12", "14"],
     },
-    { name: "Grownups(S-XXL)", 
-    size: ["XS", "S", "M", "L", "XL", "XXL"] },
+    { name: "Grownups(S-XXL)", size: ["XS", "S", "M", "L", "XL", "XXL"] },
   ]);
 
   console.log("categories seeded");
@@ -24,16 +23,9 @@ db.once("open", async () => {   //! db.once listens for the connection to the db
 
   const products = await Product.insertMany([
     {
-      name: "Aurora",
-      description: "Beautiful Aurora pattern based on Nature's wonders.",
-      image: ["/Men/19.png", "pj.png", "pants.jpg"],
-      category: categories[2]._id,
-      price: 12,
-      quantity: 500,
-    },
-    {
       name: "Topo",
-      description: "Topography pattern to guide you through your Dreamland Wonderland.",
+      description:
+        "Topography pattern to guide you through your Dreamland Wonderland.",
       image: ["/Women/36.png", "pj.png", "pants.jpg"],
       category: categories[0]._id,
       price: 10,
@@ -48,12 +40,36 @@ db.once("open", async () => {   //! db.once listens for the connection to the db
       quantity: 600,
     },
     {
+      name: "Modern",
+      category: categories[1]._id,
+      description: "pants",
+      image: ["/Kids/16.png", "pj.png", "pants.jpg"],
+      price: 25,
+      quantity: 600,
+    },
+    {
       name: "Mushroom",
       category: categories[1]._id,
       description: "Join the forest creatures with these Mushroom Jammies",
       image: ["/Baby/8.png", "pj.png", "pants.jpg"],
       price: 20,
       quantity: 600,
+    },
+    {
+      name: "Modern",
+      category: categories[1]._id,
+      description: "pants",
+      image: ["/Kids/16.png", "pj.png", "pants.jpg"],
+      price: 25,
+      quantity: 600,
+    },
+    {
+      name: "Aurora",
+      description: "Beautiful Aurora pattern based on Nature's wonders.",
+      image: ["/Men/19.png", "/patterns/19_1.jpg"],
+      category: categories[2]._id,
+      price: 12,
+      quantity: 500,
     },
     {
       name: "Dots",
@@ -87,14 +103,7 @@ db.once("open", async () => {   //! db.once listens for the connection to the db
       price: 25,
       quantity: 600,
     },
-    {
-      name: "Modern",
-      category: categories[1]._id,
-      description: "pants",
-      image: ["/Kids/16.png", "pj.png", "pants.jpg"],
-      price: 25,
-      quantity: 600,
-    },
+
     {
       name: "Modern",
       category: categories[2]._id,
@@ -103,14 +112,7 @@ db.once("open", async () => {   //! db.once listens for the connection to the db
       price: 25,
       quantity: 600,
     },
-    {
-      name: "Modern",
-      category: categories[1]._id,
-      description: "pants",
-      image: ["/Kids/16.png", "pj.png", "pants.jpg"],
-      price: 25,
-      quantity: 600,
-    },
+
     {
       name: "Modern",
       category: categories[2]._id,
