971361344a2eba4b0ccccec72c127e2bc307203d
Brodi LeBlanc <brodi.leblanc@gmail.com>
2023-07-22 21:58:23 -0700

    overriding Ivan's changes to navbar

--- client/src/components/Nav/index.js
+++ client/src/components/Nav/index.js
@@ -1,11 +1,9 @@
 import React, { useState } from "react";
 import Auth from "../../utils/auth";
 import { Link } from "react-router-dom";
-
 import { Navbar, Nav, NavDropdown, Container, Offcanvas, Button } from 'react-bootstrap';
 import '../../index.css';
-import DreamLogo from '../../images/DreamLogo.png'
-import 'bootstrap/dist/css/bootstrap.min.css';
+import Logo from "../../images/animals/lil-cutie.png";
 
 function MyNavbar() {
   const [showOffcanvas, setShowOffcanvas] = useState(false);
@@ -37,21 +35,48 @@ function MyNavbar() {
       );
     }
   }
-    //! below is the nav bar that will always render containing the site title link and the logo , it calls the showNavigation function which conditinally renders nav buttons 
+  const expand = false;
   return (
-    <header className="flex-row px-1">
+    <Navbar expand={expand} className="bg-body-tertiary mb-3 nav-background">
+      <Container fluid>
+      <div className="d-flex justify-content-center align-items-center p-3">
+              <img src={Logo} alt="Dreamland Wonderland Logo" height={50} />
+            </div>
       <h1>
-        <Link to="/">
-          <span role="img" aria-label="shopping bag">
-            <img src={Logo} width={100} height={100} />
-          </span>
-          -Dreamland
-        </Link>
+        <Navbar.Brand href="/"style={{ fontSize: '3rem', color: 'white' }}>Dreamland Wonderland</Navbar.Brand>
       </h1>
-  
-      <nav id="login">{showNavigation()}</nav>
-    
-    </header>
+        <Navbar.Toggle aria-controls={`offcanvasNavbar-expand-${expand}`} />
+        <Navbar.Offcanvas
+          id={`offcanvasNavbar-expand-${expand}`}
+          aria-labelledby={`offcanvasNavbarLabel-expand-${expand}`}
+          placement="end"
+          className="nav-background"
+        >
+          <Offcanvas.Header className="border border-dark" closeButton>
+            <Offcanvas.Title id={`offcanvasNavbarLabel-expand-${expand}`}>
+              Dreamland Wonderland
+            </Offcanvas.Title>
+          </Offcanvas.Header>
+          <Nav className="flex-column">
+            <Nav.Link href="/">Home</Nav.Link>
+            <NavDropdown title="Sizes" id="navbarScrollingDropdown" menuVariant="dark" >
+              <NavDropdown.Item href="#action3">Babies</NavDropdown.Item>
+              <NavDropdown.Item href="#action4">Kids</NavDropdown.Item>
+              <NavDropdown.Item href="#action4">Adults</NavDropdown.Item>
+              <NavDropdown.Divider />
+              <NavDropdown.Item href="#action5">All</NavDropdown.Item>
+            </NavDropdown>
+            <NavDropdown title="Account" menuVariant="dark" id="navbarScrollingDropdown">
+              <NavDropdown.Item href="/signup">Sign-up</NavDropdown.Item>
+              <NavDropdown.Item href="/login">Login</NavDropdown.Item>
+              <NavDropdown.Divider />
+              <NavDropdown.Item href="/orderHistory">Profile</NavDropdown.Item>
+            </NavDropdown>
+            <Nav.Link href="/cart">Cart</Nav.Link>
+          </Nav>
+        </Navbar.Offcanvas>
+      </Container>
+    </Navbar>
   );
 }
 
