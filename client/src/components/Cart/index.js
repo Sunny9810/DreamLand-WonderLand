16a41c69533d85eb717c8f99642e416865191272
Ivan Zavaleta <ivzavaleta@gmail.com>
2023-07-24 17:58:46 -0500

    removed a console log causing esLint error

--- client/src/components/Cart/index.js
+++ client/src/components/Cart/index.js
@@ -97,7 +97,6 @@ const Cart = () => {
       {state.cart.length ? (
         <div>
           {state.cart.map((item) => (
-            console.log(item),
             <CartItem key={item.listing} item={item} />
           ))}
 
